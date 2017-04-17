3b2f500ef5c5a389b8b20649f0fa433d71f50b86
James Costello <jcostello10@gmail.com>
2017-04-16 22:40:17 -0700

    BOT IMPROVED

--- bot.js
+++ bot.js
@@ -1,16 +1,22 @@
+//////////////////////
+///BOT LABRATORY//////
+//////////////////////
+
 console.log ('BOT IS BOOTING UP...');
 var MarkovChain = require('markovchain')
 var Twit = require('twit');
 var fs = require('fs')
+var config = require('./config');
+var T = new Twit(config)
 
-//random Int function for lenght of chain
+//random Int function for length of chain.
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-//Interval set for random time btw roughly every 20 - 40 minutes.
+//Interval set for random time.
 function getRandomTime(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -18,90 +24,23 @@ function getRandomTime(min, max) {
 }
 
 //////////////////////
-///BOT LABRATORY//////
-//////////////////////
-
-
-var config = require('./config');
-var T = new Twit(config)
-
-/////////////////////////////
-//Setting up a user stream///
-/////////////////////////////
-
-// var stream = T.stream('user');
-// stream.on('follow', followed);
-// function followed(){
-// }
-
-////////////////
-// Get request//
-////////////////
-
-// var params = {
-//   q: 'danzig',
-//   count: 2
-// }
-
-// T.get('search/tweets', params, gotData);
-
-// function gotData(err, data, response ){
-//   var tweets = data.statuses;
-//   for(var i = 0; i < tweets.length; i++){
-//   console.log(tweets[i].text);
-//   }
-// }
-
-//////////////////////
 ////MARKOV CHAINING///
 //////////////////////
 
-//////////////////////
-//Example of Post();//
-//////////////////////
+var useUpperCase = function(wordList) {
+  var tmpList = Object.keys(wordList).filter(function(word) {
+    return word[0] >= 'A' && word[0] <= 'Z' || word[0] >= 'a' && word[0] <= 'z'
+  })
+  return tmpList[~~(Math.random()*tmpList.length)]
+}
 
 setInterval(function() {
     var quotes = new MarkovChain(fs.readFileSync('./quotes.txt', 'utf8'))
-    var snippet = quotes.start('The').end(getRandomInt(4,18)).process();
+    var snippet = quotes.start(useUpperCase).end(getRandomInt(5,18)).process();
       console.log(snippet)
 
-      var tweetIt = {
-        status: snippet
-      }
-
+      var tweetIt = { status: snippet }
 
       T.post('statuses/update', tweetIt, tweeted);
-
-      function tweeted(err, data, response){
-        if(err) {
-         console.log("Uh oh:  ", err)
-        } else {
-         console.log("it worked")
-        }
-      }
- }, 4000)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      function tweeted(err, data, response){ err ? console.log("Uh oh:  ", err) : console.log("BOT (⌐■_■) BOT"); }
+ }, getRandomTime(500 * 9, 500 * 20) )
